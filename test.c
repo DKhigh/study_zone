c028f9b9db632ed473d4aa122731c0a26df3e986
Lysoo <lolo890033@naver.com>
2026-04-02 10:56:32 +0900

    Add Stack

--- test.c
+++ test.c
@@ -1,8 +1,43 @@
 ﻿#include <stdio.h>
+#define MAX_SIZE 100
+typedef int element;
+
+typedef struct stack {
+    element arr[MAX_SIZE];
+    int top;
+}Stack;
+
+void init(Stack *s) {
+    s->top = -1;
+}
+
+int is_full(Stack *s) {
+    return (s->top == MAX_SIZE - 1);
+}
+
+int is_empty(Stack *s) {
+    return (s->top == -1);
+}
+
+void push(Stack *s, element value) {
+    if(is_full(s)) {
+        printf("stack is full\n");
+        return ;
+    }
+    s->arr[++(s->top)] = value;
+}
+
+element pop(Stack *s) {
+    if(is_empty(s)) {
+        printf("stack is empty");
+        return 0;
+    }
+    return (s->arr[(s->top)--])
+}
 
 int main() {
 
-
+    
     
     return 0;
 }
